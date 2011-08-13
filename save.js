ee6023f76843a3e063b7b80fbd7ca0ede141a9b2
Sai Charan <scharan20@gmail.com>
2011-08-12 20:09:52 -0700

    Removed dependence on user specific token.

--- save.js
+++ save.js
@@ -19,8 +19,8 @@
     try {
         if(!b)
             throw(0);
-        d.title='(Saving...) '+d.title;
-        z.setAttribute('src',l.protocol+'//www.instapaper.com/j/<FROM-BOOKMARKLET>?u='+encodeURIComponent(l.href)+'&t='+(new Date().getTime()));
+        //z.setAttribute('src',l.protocol+'//www.instapaper.com/j/<FROM-BOOKMARKLET>?u='+encodeURIComponent(l.href)+'&t='+(new Date().getTime()));
+        z.setAttribute('src',l.protocol+'//www.instapaper.com/hello2?u='+encodeURIComponent(l.href)+'&t='+d.title);
         b.appendChild(z);
     } catch(e){
         alert('Please wait until the page has loaded.');
