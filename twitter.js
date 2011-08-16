3b8bcbaffdc7a4b5822cca6c536a0caf36962e5b
Sai Charan <scharan@cs.ucr.edu>
2011-08-16 10:13:41 -0700

    Round-about way to get twitter working on my tumblr. Tumblr engine mucks up the widget code that Twitter provides.

---
+++ twitter.js
@@ -0,0 +1,30 @@
+(function(){
+    new TWTR.Widget({
+        version: 2,
+        type: 'profile',
+        rpp: 5,
+        interval: 6000,
+        width: 250,
+        height: 300,
+        theme: {
+            shell: {
+                background: '#262626',
+                color: '#d6dbdf'
+            },
+            tweets: {
+                background: '#262626',
+                color: '#d6dbdf',
+                links: '#ffbb00'
+            }
+        },
+        features: {
+            scrollbar: false,
+            loop: false,
+            live: true,
+            hashtags: true,
+            timestamp: true,
+            avatars: false,
+            behavior: 'all'
+        }
+    }).render().setUser('scharan').start();
+}());
