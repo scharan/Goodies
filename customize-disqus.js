f52d00dfeee343d44713e2edb781bffa9d0fe881
Sai Charan <scharan@cs.ucr.edu>
2011-08-16 12:42:35 -0700

    Initial version: blindly hides disqus on all pages.

---
+++ customize-disqus.js
@@ -0,0 +1,10 @@
+// Author: Sai Charan K.
+// Depends on jQuery (currently tested with version 1.6.2)
+// 
+// Aims to:
+// 1. Disable disqus on certain pages.
+// 2. 
+
+$(document).ready(function() {
+    $("#disqus_thread").hide();
+});
