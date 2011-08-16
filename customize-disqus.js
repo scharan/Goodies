1f3b3e436be598fc589f10d140e4514050dc99fb
Sai Charan <scharan@cs.ucr.edu>
2011-08-16 13:30:29 -0700

    Added license and headers.

--- customize-disqus.js
+++ customize-disqus.js
@@ -1,9 +1,14 @@
 // Author: Sai Charan K.
+// Distributed without warranties or conditions of any kind, either
+// express or implied except those under the CC BY-NC 3.0 license.
+// You may obtain a copy of the license at:
+//
+// http://creativecommons.org/licenses/by-nc/3.0/
+
 // Depends on jQuery (currently tested with version 1.6.2)
 // 
 // Aims to:
 // 1. Disable disqus on certain pages.
-// 2. 
 
 $(document).ready(function() {
     var nodsq = ["ask", "about"];
